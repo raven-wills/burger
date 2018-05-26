d423a386fe43c3cb77a58ed1f4b8901951e204ef
Raven Wills <raven.wills@gmail.com>
2018-05-26 18:06:54 -0500

    Complete up without the devour button working

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,12 @@
 -- id: an auto incrementing int that serves as the primary key.
 -- burger_name: a string.
 -- devoured: a boolean.
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(50),
+devoured BOOLEAN DEFAULT FALSE
+);
